15d8ffdab88babdee04ad51ef775d00ea1dc14d7
Ray John Alovera <ray.john.e.alovera@accenture.com>
2017-09-13 11:49:29 +0800

    - sign-in form html

---
+++ src/components/SignIn.vue
@@ -0,0 +1,39 @@
+<template>
+    <div class="container">
+        <h1>Welcome</h1>
+        <b-form @submit="onSubmit">
+            <b-form-group id="grpEmail" label="Email" label-for="grpEmail">
+                <b-form-input id="txt-email" type="email" v-model="form.email" placeholder="Email" required="required"></b-form-input>
+            </b-form-group>
+            <b-form-group id="grpPass" label="Password" label-for="grpPass">
+                <b-form-input id="txt-pass" type="password" v-model="form.password" placeholder="Password" required="required"></b-form-input>
+            </b-form-group>
+            <div class="float-right">
+                <b-button type="reset" variant="secondary">Reset</b-button>
+                <b-button type="submit" variant="primary">Submit</b-button>
+            </div>
+        </b-form>
+    </div>
+</template>
+
+<script>
+export default {
+  data: () => {
+    return {
+      form: {
+       email: ''
+      }
+    }
+  },
+  methods: {
+    onSubmit(evt) {
+      evt.preventDefault();
+      console.log(JSON.stringify(this.form));
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
