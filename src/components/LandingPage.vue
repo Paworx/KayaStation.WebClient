e77a0f5034c1fb635f10a9bb1599ac982b04f9a8
Ray John Alovera <ray.john.e.alovera@accenture.com>
2017-09-27 12:41:00 +0800

    - create signup form

--- src/components/LandingPage.vue
+++ src/components/LandingPage.vue
@@ -6,7 +6,7 @@
             <b-collapse is-nav id="nav_collapse">
 
                 <b-nav is-nav-bar class="ml-auto">
-                    <b-nav-item href="#">Create an Account</b-nav-item>
+                    <b-nav-item href="#" @click="onCreate()">Create an Account</b-nav-item>
                     <b-nav-item to="/signin">Sign In</b-nav-item>
                 </b-nav>
                
@@ -15,11 +15,22 @@
         <b-jumbotron header="Kaya Station" lead="Satisfaction. In Reach">
 
         </b-jumbotron>
+        <sign-up ref="sign_up_component" name="sign_up_modal"></sign-up>
     </div>
 </template>
 
 <script>
-
+import SignUp from './SignUp.vue'
+export default {
+    methods: {
+        onCreate() {
+            this.$refs.sign_up_component.$refs.sign_up_modal.show();
+        }
+    },
+    components: {
+        SignUp
+    }
+}
 </script>
 
 <style>
