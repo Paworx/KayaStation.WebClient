6f409be32d6edf8a52dedd8fc094e8871da8863f
Ray John Alovera <ray.john.e.alovera@accenture.com>
2017-10-11 12:41:09 +0800

    - put logout controls in toolbar

--- src/store/modules/user/index.js
+++ src/store/modules/user/index.js
@@ -10,12 +10,19 @@ const user = {
             let { email, name } = credentials;
             state.email = email;
             state.name = name;
+        },
+        logout(state) {
+            state.email = '';
+            state.name = '';
         }
     },
     actions: {
         async signin ({state, commit}, user) {
             let credentials = await UserApi.signin(user);
             commit('signin', credentials);
+        },
+        async logout({state, commit, rootState}){
+            commit('logout');
         }
     }
 }
