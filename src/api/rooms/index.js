ee4e406dd606e9f79007ed1dc6cae01a5cc20449
Ray John Alovera <ray.john.e.alovera@accenture.com>
2017-10-19 19:52:40 +0800

    - add room through API - initialize Headers as object so tokens can be assigned later

--- src/api/rooms/index.js
+++ src/api/rooms/index.js
@@ -3,17 +3,16 @@ import { RoomType } from './types';
 
 const RoomsApi = {
     currId: 0,
-    headers: new Headers({
+    headers: {
         'Content-Type': 'application/json'
-        
-    }),
+    },
     baseUrl: 'http://localhost:49690/api/v1/Rooms/',
     async getById(id) {
         let url = this.baseUrl + 'GetById/' + id
         let tokens = await Auth.getAccessToken()
-        let headers = Object.assign({}, this.headers, {
+        let headers = new Headers(Object.assign({}, this.headers, {
             'Authorization': 'Bearer ' + tokens.requestToken
-        })
+        }))
         let params = {
             method: 'GET',
             headers: headers
@@ -26,10 +25,23 @@ const RoomsApi = {
         return rooms
     },
     async add(room){
-        let roomWithId = Object.assign({}, room, {
-            Id: ++ this.currId
+        let url = this.baseUrl + 'Add'
+        let tokens = await Auth.getAccessToken()
+        let headers = new Headers(Object.assign({}, this.headers, {
+            'Authorization': 'Bearer ' + tokens.requestToken
+        }))
+        let params = {
+            method: 'POST',
+            headers: headers,
+            body: JSON.stringify(room)
+        }
+        console.log(params)
+        let req = new Request(url, params)
+        let res = fetch(req)
+        let savedRoom = await res.then(response => {
+            return response.json().then(room => room, err => [])
         })
-        return Promise.resolve(roomWithId)
+        return savedRoom
     }
 }
 
