7ec5cf5f8fa64fd8ac68597b84bf1070ce96c752
Ray John Alovera <ray.john.e.alovera@accenture.com>
2017-10-20 11:13:29 +0800

    - moved modal to own component - clear form after submitting

---
+++ src/components/RoomModal.vue
@@ -0,0 +1,48 @@
+<template>
+    <b-modal ref="room_add_modal" title="Add Room" ok-title="Add" close-title="Cancel"
+        @ok="onSubmit" >
+
+        <b-form>
+            <b-form-group label="Name:">
+                <b-form-input type="text" v-model="form.name" required></b-form-input>
+            </b-form-group>
+            <b-form-group label="Type:">
+                <b-form-input type="text" v-model="form.type" required></b-form-input>
+            </b-form-group>
+            <b-form-group label="Price:">
+                <b-form-input type="number" v-model="form.price" required></b-form-input>
+            </b-form-group>
+        </b-form>
+    </b-modal>
+</template>
+
+<script>
+export default {
+    data: () => {
+        return {
+            form: {
+                name: '',
+                price: '',
+                type: ''
+            }
+        }
+    },
+    methods: {
+        onSubmit() {
+            let room = Object.assign({}, this.form, {
+                hotelId: this.$store.state.hotels.currentHotel.id
+            })
+            this.form = {
+                name: '',
+                price: '',
+                type: ''
+            }
+            this.$store.dispatch('rooms/add', room);
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
